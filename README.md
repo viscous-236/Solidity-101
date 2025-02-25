6453d57fdf400ff64794578527da4a2452e43834
viscous-236 <vaibhavgoyal2310066@gmail.com>
2025-02-25 18:58:59 +0530

    deployed the first contract

---
+++ README.md
@@ -0,0 +1,2 @@
+#NOTES
+Link for the notes: https://www.notion.so/Solidity-101-1a40b6d17b5c80088fe6ea1284848a3d?pvs=4
---
+++ SimpleStorage.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18; // Starting line of the solidity specifying the version
+// the '^' specify the version equal to or greater than the specified will work
+// pragma solidity >=0.8.18 <0.9.0 any version between the two specified
+
+contract SimpleStorage{
+    // Basic types: boolean,uint,int,address,bytes
+    // bool hasFavouriteNumber=true;
+    // uint256 favouriteNumber=88;// uint256=uint
+    // int256 favouriteNum = -88;// int256=int
+    // address myAddress= 0x33C958aB751734E6722C91B3AE89DA026D5B2AF8;
+    // bytes32 favuriteAnimal="Dog";// max bytes 32
+    uint256 myFavouriteNumber; // 0
+
+    function store(uint256 _favouriteNumber) public {
+        myFavouriteNumber=_favouriteNumber;
+    }
+
+    function retrieve() public view returns(uint256){
+        return myFavouriteNumber;
+    }
+
+    // uint256[] listOfFavouriteNumbers;
+    struct Person{
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // Person public vai = Person(10,"Vai");
+    // Person public vai = Person({favouriteNumber: 10,name:"Vai"});
+
+    //Dynamic Array
+    Person[] public listOfPeople;
+
+    //Static array
+    // Person[2] public listofpeople;
+
+    function addPerson(string memory _name,uint256 _favouriteNumber) public {
+        // Person memory newPerson=Person(_favouriteNumber,_name);
+        // listOfPeople.push(newPerson);
+        listOfPeople.push(Person(_favouriteNumber,_name));
+    }
+}
