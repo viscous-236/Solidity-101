8ab2d9b23b9696e352175e8c6ab542461716fe05
viscous-236 <vaibhavgoyal2310066@gmail.com>
2025-03-03 16:49:29 +0530

    3/3/25

--- FundMe.sol
+++ FundMe.sol
@@ -3,8 +3,14 @@
 // Set a minimum funding value in USD
 
 // SPDX-License-Identifier:MIT
+
 pragma solidity ^0.8.18;
 
 contract FundMe{
-    
+    function fund() public payable {
+        require(msg.value > 1e18,"Didn't send enough ETH!"); // 1e18 = 1ETH
+    }
+
+
+    // function withdraw() public {}
 }
