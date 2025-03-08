44c599b13a4c6b22a3eb07a0ad040bc7f916ee0d
viscous-236 <vaibhavgoyal2310066@gmail.com>
2025-03-08 23:19:34 +0530

    8/3/25

--- FundMe.sol
+++ FundMe.sol
@@ -7,8 +7,11 @@
 pragma solidity ^0.8.18;
 
 contract FundMe{
+
+    uint256 minimumDollar = 5;
+
     function fund() public payable {
-        require(msg.value > 1e18,"Didn't send enough ETH!"); // 1e18 = 1ETH
+        require(msg.value > minimumDollar,"Didn't send enough ETH!"); // 1e18 = 1ETH
     }
     // function withdraw() public {}
 }
