7086b5584f6f107f5d8b4f3d841d055cfc5713f6
viscous-236 <vaibhavgoyal2310066@gmail.com>
2025-03-09 13:06:20 +0530

    addes notes

--- FundMe.sol
+++ FundMe.sol
@@ -10,7 +10,8 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe{
 
-    uint256 minimumDollar = 5e18;
+    uint256 minimumDollar = 5e18; // we multiply it by e18 as the getConversionRate
+    // will give the result is e18 decimal points
 
     function fund() public payable {
         require(getConversionRate(msg.value) >= minimumDollar,"Didn't send enough Dollars!"); // 1e18 = 1ETH
@@ -19,18 +20,19 @@ contract FundMe{
 
     function getPrice() public view returns(uint256){
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI 
+        // ABI ✅
         
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (, int256 answer,,,) = priceFeed.latestRoundData();
         // Price of Eth in terms oF USD
-        // 2000.00000000
-        return uint256(answer * 1e10);
+        // 2000.00000000 has 10 decimal points so multiply it by e10
+        return uint256(answer * 1e10);// int has to be converted into uint256
     }   
 
     function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPriceInUSD = getPrice();
+        uint256 ethPriceInUSD = getPrice();// will give the price of ETH in USD
         uint256 totalEthPriceInUSD = (ethPriceInUSD * ethAmount) / 1e18;
+        // 1e18*1e18/1e18
         return totalEthPriceInUSD;
     }
 }
