9a68b5d1f36f34011055cbbafb66604c65b9d4de
viscous-236 <vaibhavgoyal2310066@gmail.com>
2025-03-09 23:30:49 +0530

    form the library PriceConverter which stores all the calculation which are initially in FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -5,34 +5,24 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 
 contract FundMe{
 
+    using PriceConverter for uint256;
+
     uint256 minimumDollar = 5e18; // we multiply it by e18 as the getConversionRate
     // will give the result is e18 decimal points
 
+    address[] public funders;// arrays of address of people who send the money
+    mapping(address funder => uint256 amountFunded) public addressToFundedAmount;
+
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumDollar,"Didn't send enough Dollars!"); // 1e18 = 1ETH
+        require(msg.value.getConversionRate() >= minimumDollar,"Didn't send enough Dollars!"); // 1e18 = 1ETH
+        funders.push(msg.sender);// msg.sender is the default value to contain addresses 
+        addressToFundedAmount[msg.sender] = msg.value + addressToFundedAmount[msg.sender];
     }
     // function withdraw() public {}
 
-    function getPrice() public view returns(uint256){
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI ✅
-        
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 answer,,,) = priceFeed.latestRoundData();
-        // Price of Eth in terms oF USD
-        // 2000.00000000 has 10 decimal points so multiply it by e10
-        return uint256(answer * 1e10);// int has to be converted into uint256
-    }   
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPriceInUSD = getPrice();// will give the price of ETH in USD
-        uint256 totalEthPriceInUSD = (ethPriceInUSD * ethAmount) / 1e18;
-        // 1e18*1e18/1e18
-        return totalEthPriceInUSD;
-    }
 }
---
+++ PriceConverter.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter{
+    function getPrice() internal view returns(uint256){
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI ✅
+        
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 answer,,,) = priceFeed.latestRoundData();
+        // Price of Eth in terms oF USD
+        // 2000.00000000 has 10 decimal points so multiply it by e10
+        return uint256(answer * 1e10);// int has to be converted into uint256
+    }   
+
+    function getConversionRate(uint256 ethAmount) internal  view returns(uint256){
+        uint256 ethPriceInUSD = getPrice();// will give the price of ETH in USD
+        uint256 totalEthPriceInUSD = (ethPriceInUSD * ethAmount) / 1e18;
+        // 1e18*1e18/1e18
+        return totalEthPriceInUSD;
+    }
+
+    function getVersion() internal view  returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+}
