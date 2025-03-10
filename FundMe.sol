4660d630c741149dbbcc6ac9b22767e5d16fc88b
viscous-236 <vaibhavgoyal2310066@gmail.com>
2025-03-10 16:42:32 +0530

    complete with withdraw

--- FundMe.sol
+++ FundMe.sol
@@ -18,12 +18,19 @@ contract FundMe{
     address[] public funders;// arrays of address of people who send the money
     mapping(address funder => uint256 amountFunded) public addressToFundedAmount;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumDollar,"Didn't send enough Dollars!"); // 1e18 = 1ETH
         funders.push(msg.sender);// msg.sender is the default value to contain addresses 
         addressToFundedAmount[msg.sender] += msg.value;
     }
-    function withdraw() public {
+    function withdraw() public onlyOwner {
+        // require(msg.sender == owner,"Must be owner"); add modifiuer instead
         for (uint256 fundersIndex=0; fundersIndex<funders.length; fundersIndex++) 
         {
             address funder = funders[fundersIndex];
@@ -33,11 +40,17 @@ contract FundMe{
         funders = new address[](0);
         // withdraw the function
         // transfer 
-        payable(msg.sender).transfer(address(this).balance);
-        // address refers to adress of the person belongi
+        payable(msg.sender).transfer(address(this).balance);// address refers to adress of the person belongi
         // send  
+        bool sendSuccessMsg = payable(msg.sender).send(address(this).balance);
+        require(sendSuccessMsg,"Send Falied");
         // call
-         
+        // (bool callSuccessMsg, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccessMsg,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccessMsg,"Call Failed");
     }
-
+    modifier onlyOwner(){
+        require(msg.sender == owner,"Must be Owner");
+        _;
+    }
 }
